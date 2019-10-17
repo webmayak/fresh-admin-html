a8ae6176450b922bddc8bed269d49c7435d0898f
aileen <aileen.kingsler@mail.ru>
2019-10-17 14:51:30 +1000

    Сверстала страницу Tickets

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,3 +1,9 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
+
+  $('.btn--toggle-filters').click(function () {
+    $('.main__filters').toggle();
+    $('.main__content').toggleClass('main__content--fullwidth');
+    $(this).toggleClass('btn--show').attr('title', $(this).hasClass('btn--show') ? 'Show filters' : 'Hide filters');;
+  });
 });
