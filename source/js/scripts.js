cc2658457b7991bf67b76c9213e8a55add65ba04
aileen <aileen.kingsler@mail.ru>
2019-10-18 15:40:47 +1000

    Сверстала страницу просмотра ticket

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -3,7 +3,7 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip();
 
   $('.btn--toggle').click(function () {
-    $('.main__details--first').toggle();
+    $('.main__details--collapsed').toggle();
     $('.main__content').toggleClass('main__content--fullwidth');
     $(this).toggleClass('btn--show').attr('title', $(this).hasClass('btn--show') ? 'Expand' : 'Collapse');;
   });
